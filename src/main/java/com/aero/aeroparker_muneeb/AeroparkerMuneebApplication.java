cf281f77dbff9ea1eff20e05e2bc1ddc45eae798
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-11-22 16:20:21 +0000

    First Commit

---
+++ src/main/java/com/aero/aeroparker_muneeb/AeroparkerMuneebApplication.java
@@ -0,0 +1,13 @@
+package com.aero.aeroparker_muneeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroparkerMuneebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroparkerMuneebApplication.class, args);
+	}
+
+}
